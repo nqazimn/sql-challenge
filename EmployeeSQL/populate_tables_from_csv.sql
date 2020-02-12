4c3dfc9106d31c4cd5dfbb608393659d0dfa1a9f
nqazimn <nqazimn@gmail.com>
2020-02-12 09:17:46 -0600

    updated csv population schema

--- EmployeeSQL/populate_tables_from_csv.sql
+++ EmployeeSQL/populate_tables_from_csv.sql
@@ -26,16 +26,16 @@ WITH (FORMAT CSV, HEADER);
 SELECT *
 FROM salaries;
 
-COPY dept_manager
-FROM 'D:\BOOTCAMP\git_repos\sql-challenge\data\dept_manager.csv'
-WITH (FORMAT CSV, HEADER);
-
-SELECT *
-FROM dept_manager;
-
 COPY dept_emp
 FROM 'D:\BOOTCAMP\git_repos\sql-challenge\data\dept_emp.csv'
 WITH (FORMAT CSV, HEADER);
 
 SELECT *
 FROM dept_emp;
+
+COPY dept_manager
+FROM 'D:\BOOTCAMP\git_repos\sql-challenge\data\dept_manager.csv'
+WITH (FORMAT CSV, HEADER);
+
+SELECT *
+FROM dept_manager;
